3e0439dee2dedb042be9aa93595b185569376b3c
Andy Smith <andy.smith@90poe.io>
2021-05-27 12:15:43 +0100

    Use secret store

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,20 @@ provider "google" {
   zone   = "europe-west2-a"
 }
 
+data "google_secret_manager_secret_version" "message" {
+  secret = "message"
+}
+
 resource "google_cloud_run_service" "default" {
   name     = "terraform-hello-world"
   location = "europe-west2"
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/ingress" = "internal"
+      "run.googleapis.com/ingress-status" = "internal"
+    }
+  }
 
   template {
     spec {
@@ -13,7 +24,7 @@ resource "google_cloud_run_service" "default" {
         image = "eu.gcr.io/core-advice-314810/hello-world:v0.0.1"
         env {
           name = "MESSAGE"
-          value = "Hello Everybody!"
+          value = data.google_secret_manager_secret_version.message.secret_data 
         }
       }
     }
